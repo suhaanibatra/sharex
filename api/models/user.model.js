8ecae9539b1bd09f7e9ad574f71778da47890d27
Suhaani Batra <suhaanibatra23@gmail.com>
2024-06-19 22:50:48 +0530

    add admin status to user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhP1LzsEOSiEWX4xedVLb8maKpMnHCUpdtNQ&s',
     },
+    isAdmin:{
+        type: Boolean,
+        default: false,
+    },
 }, {timestamps: true}
 );
 
