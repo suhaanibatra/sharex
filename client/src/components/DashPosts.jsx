0c2291b0889cd681d3ef79a4e54075ab4502e823
Suhaani Batra <suhaanibatra23@gmail.com>
2024-07-26 13:07:53 +0530

    add show more functionality to the posts dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
+  const [showMore, setShowMore] = useState(true);
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
@@ -13,12 +14,34 @@ function DashPosts() {
 
       if (res.ok) {
         setUserPosts(data.posts);
+        if(data.posts.length < 9){
+          setShowMore(false);
+        }
       }
     };
     if (currentUser.isAdmin) {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -76,6 +99,15 @@ function DashPosts() {
               </Table.Body>
             ))}
           </Table>
+
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='w-full text-teal-500 self-center text-sm py-7'
+            >
+              Show more
+            </button>
+          )}
         </>
       ) : (
         <p>You have no posts</p>
