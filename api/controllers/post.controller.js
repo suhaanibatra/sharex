8d85313267e579ea05e431d0e13b84432b9077f9
Suhaani Batra <suhaanibatra23@gmail.com>
2024-07-27 13:25:06 +0530

    add update post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -86,3 +86,32 @@ export const deletepost = async(req, res, next) =>{
     next(error);
   }
 }
+
+export const updatepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to update this post'));
+  }
+
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        $set: {
+          title: req.body.title,
+          content: req.body.content,
+          category: req.body.category,
+          image: req.body.image,
+          slug: req.body.title
+          .split(" ")
+          .join("-")
+          .toLowerCase()
+          .replace(/[^a-zA-Z0-9-]/g, ""),
+        },
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
+};
